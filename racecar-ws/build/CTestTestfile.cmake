9fa772a84fee1489e9b52670f07e30d54db35fe8
Your Name <you@example.com>
2018-06-14 13:48:56 +0800

    meterial

---
+++ racecar-ws/build/CTestTestfile.cmake
@@ -0,0 +1,25 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/racecar-ws/src
+# Build directory: /home/ubuntu/racecar-ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(racecar/racecar)
+subdirs(teb_local_planner_tutorials)
+subdirs(vesc/vesc)
+subdirs(vesc/vesc_msgs)
+subdirs(racebot)
+subdirs(rplidar_ros)
+subdirs(TA_example_labs/ta_lab3)
+subdirs(TA_example_labs/ta_lab4)
+subdirs(racecar/ackermann_cmd_mux)
+subdirs(razor_imu_9dof)
+subdirs(particle_filter)
+subdirs(TA_example_labs/ta_lab5)
+subdirs(TA_example_labs/ta_lab6)
+subdirs(robot_localization)
+subdirs(racebot_description)
+subdirs(vesc/vesc_ackermann)
+subdirs(vesc/vesc_driver)
+subdirs(zed-ros-wrapper-master)
---
+++ racecar-ws/devel/share/ta_lab4/cmake/ta_lab4-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ta_lab4_MESSAGE_FILES "/home/ubuntu/racecar-ws/src/TA_example_labs/ta_lab4/msg/Detection.msg")
+set(ta_lab4_SERVICE_FILES "")
